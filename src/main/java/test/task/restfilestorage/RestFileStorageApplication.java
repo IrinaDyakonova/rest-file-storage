40d2310afc09adf5550ae8f33aaff533458d034b
IrinaDyakonova <dyakonova.irina.v@gmail.com>
2020-08-14 10:56:35 +0300

    new project

---
+++ src/main/java/test/task/restfilestorage/RestFileStorageApplication.java
@@ -0,0 +1,13 @@
+package test.task.restfilestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFileStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestFileStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/test/task/restfilestorage/RestFileStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package test.task.restfilestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFileStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
